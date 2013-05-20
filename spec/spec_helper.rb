cc9151351cfd0966261dfdb5b87e16ac5582bfe4
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-05-20 15:55:47 +1000

    Loads factory file from spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,14 +8,10 @@ require 'rspec/autorun'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+require 'factories/factories'
+
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.include FactoryGirl::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
