ad06b2be197f5f62af6f8c281bca6d227164f354
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-05-23 11:37:07 +1000

    Makes Question.required field accessible.

--- app/models/surveyable/question.rb
+++ app/models/surveyable/question.rb
@@ -14,7 +14,7 @@ module Surveyable
 
     validates :content, :field_type, presence: true
 
-    attr_accessible :content, :field_type, :survey, :answers_attributes
+    attr_accessible :content, :field_type, :survey, :answers_attributes, :required
 
     accepts_nested_attributes_for :answers, allow_destroy: true, reject_if: lambda { |a| a[:content].blank? }
   end
--- spec/models/surveyable/question_spec.rb
+++ spec/models/surveyable/question_spec.rb
@@ -8,7 +8,7 @@ module Surveyable
     it { should have_many(:answers).dependent(:destroy) }
     it { should accept_nested_attributes_for(:answers).allow_destroy(true) }
 
-    %w( content field_type survey answers_attributes ).each do |attr|
+    %w( content field_type survey required answers_attributes ).each do |attr|
       it { should allow_mass_assignment_of(attr.to_sym) }
     end
   end
