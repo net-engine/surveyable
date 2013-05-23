9a23ea6c2bcc17b667bbec4d416f6b094ae696ab
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-05-23 11:38:40 +1000

    Removes API from routes. Makes routes be drawn from Rails.application instead.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
-Surveyable::Engine.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :surveys, except: [:new, :edit]
-    end
-  end
-
+Rails.application.routes.draw do
   get '/surveys/:access_token' => 'responses#show'
   post '/surveys/:access_token' => 'responses#show', as: :submit_survey
+
+  namespace :surveyable do
+    resources :surveys
+  end
 end
