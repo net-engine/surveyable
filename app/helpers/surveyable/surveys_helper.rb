07167177240b02c24a205842fd0f885695a2482c
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-05-31 08:46:15 +1000

    Add button must load 4 answers, not 3.

--- app/helpers/surveyable/surveys_helper.rb
+++ app/helpers/surveyable/surveys_helper.rb
@@ -7,7 +7,7 @@ module Surveyable
     def link_to_add_fields(name, form, association)
       new_object = form.object.class.reflect_on_association(association).klass.new
 
-      3.times { new_object.answers.build } if new_object.class.to_s == 'Surveyable::Question'
+      4.times { new_object.answers.build } if new_object.class.to_s == 'Surveyable::Question'
 
       fields = form.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
         render(association.to_s.singularize + "_fields", form: builder)
