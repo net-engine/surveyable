2367211a19409aedc54bfa11ed598beea359e3c6
Olivier Pichon <olivier@netengine.com.au>
2013-08-30 15:35:14 +1000

    See Trigger #11073: Changed PieCharts colour

--- app/assets/javascripts/surveyable/report_handler.js
+++ app/assets/javascripts/surveyable/report_handler.js
@@ -4,8 +4,7 @@
     var width = 960,
       height = 500,
       radius = Math.min(width, height) / 2;
-    var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    var color = d3.scale.category20c();
     var arc = d3.svg.arc()
       .outerRadius(radius - 10)
       .innerRadius(0);
