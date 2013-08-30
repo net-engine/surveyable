7cd2423154677b57975063ecf3232d35fc10d55e
Olivier Pichon <olivier@netengine.com.au>
2013-08-30 16:53:22 +1000

    See Trigger #11073: Unit tests and javascript slight correction.

--- app/assets/javascripts/surveyable/report_handler.js
+++ app/assets/javascripts/surveyable/report_handler.js
@@ -1,36 +1,40 @@
 (function(w){
 
   var draw = function($pieChart, results){
-    var width = 960,
-      height = 500,
-      radius = Math.min(width, height) / 2;
-    var color = d3.scale.category20c();
-    var arc = d3.svg.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(0);
-    var svg = d3.select($pieChart.toArray()[0]).append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    if (results.length === 0){
+      $pieChart.html("<p>No Data Available at the moment</p>");
+    }else{
+      var width = 960,
+        height = 500,
+        radius = Math.min(width, height) / 2;
+      var color = d3.scale.category20c();
+      var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+      var svg = d3.select($pieChart.toArray()[0]).append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var pie = d3.layout.pie()
-      .value(function(d) { return d.percentage; });
+      var pie = d3.layout.pie()
+        .value(function(d) { return d.percentage; });
 
-    var g = svg.selectAll(".arc")
-      .data(pie(results))
-      .enter().append("g")
-      .attr("class", "arc");
+      var g = svg.selectAll(".arc")
+        .data(pie(results))
+        .enter().append("g")
+        .attr("class", "arc");
 
-    g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.percentage); });
+      g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.percentage); });
 
-    g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.text + " (" + d.data.occurrence + ") " + d.data.percentage + "%" ; });
+      g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.text + " (" + d.data.occurrence + ") " + d.data.percentage + "%" ; });
+    }
   };
 
 
--- spec/models/surveyable/question_spec.rb
+++ spec/models/surveyable/question_spec.rb
@@ -21,5 +21,50 @@ module Surveyable
         end
       end
     end
+
+    describe "#reports" do
+      context "when the question is not a reportable question" do
+        let!(:question){ create(:question) }
+        it "should return a 'N/A' string" do
+          question.reportable_question?.should == false
+          question.reports.should == 'N/A'
+        end
+      end
+
+      context "when the question is reportable but there was no response provided for that survey" do
+        let!(:survey){ create(:survey_with_questions_and_answers, questions_count: 2) }
+        it "should return an empty array" do
+          question = survey.questions.first
+          question.reportable_question?.should == true
+          ResponseAnswer.where(question_id: question.id).should be_empty
+          question.reports.should == []
+        end
+      end
+
+      context "when the question is reportable and where there was a provided response" do
+        let!(:survey)   { create(:survey_with_questions_and_answers, questions_count: 1) }
+        let!(:response) { create(:response, survey: survey) }
+        let!(:ra1)      { { response: response, question: survey.questions.first, answer: survey.questions.first.answers.first } }
+        let!(:ra2)      { { response: response, question: survey.questions.first, answer: survey.questions.first.answers[1] } }
+        let!(:ra3)      { { response: response, question: survey.questions.first, answer: survey.questions.first.answers[1] } }
+
+        before do
+          response.response_answers.create(ra1)
+          response.response_answers.create(ra2)
+          response.response_answers.create(ra3)
+        end
+
+        it "should return the correct result array" do
+          question = survey.questions.first
+          question.reportable_question?.should == true
+          ResponseAnswer.where(question_id: question.id).should_not be_empty
+          question.reports.should == [
+            # 2/3
+            {:text=>survey.questions.first.answers[1].content, :occurrence=>2, :percentage=>66.67, :id=>survey.questions.first.answers[1].id},
+            # 1/3
+            {:text=>survey.questions.first.answers[0].content, :occurrence=>1, :percentage=>33.33, :id=>survey.questions.first.answers[0].id}]
+        end
+      end
+    end
   end
 end
