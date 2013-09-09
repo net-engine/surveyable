074ce1fc78e4725ee870d42bcb9bca978d030cb5
Brad Parker <hi@bradparker.com>
2013-09-09 15:14:02 +1000

    Switches from D3 to morris js

--- app/assets/javascripts/surveyable/report_handler.js
+++ app/assets/javascripts/surveyable/report_handler.js
@@ -1,57 +1,34 @@
 (function(w){
 
-  var draw = function($pieChart, results){
-    if (results.length === 0){
-      $pieChart.html("<p>No Data Available at the moment</p>");
-    }else{
-      var width = 960,
-        height = 500,
-        radius = Math.min(width, height) / 2;
-      var color = d3.scale.category20c();
-      var arc = d3.svg.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-      var svg = d3.select($pieChart.toArray()[0]).append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-      var pie = d3.layout.pie()
-        .value(function(d) { return d.percentage; });
-
-      var g = svg.selectAll(".arc")
-        .data(pie(results))
-        .enter().append("g")
-        .attr("class", "arc");
-
-      g.append("path")
-        .attr("d", arc)
-        .style("fill", function(d) { return color(d.data.percentage); });
-
-      g.append("text")
-        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-        .attr("dy", ".35em")
-        .style("text-anchor", "middle")
-        .text(function(d) { return d.data.text + " (" + d.data.occurrence + ") " + d.data.percentage + "%" ; });
+  var drawGraph = function(type, data, $element) {
+    var graph = new Morris[type] {
+      element: $element,
+      data: data,
+      hideHover: 'auto',
+      fillOpacity: 0.2,
+      grid: false,
     }
-  };
 
+    window.onresize = function() {
+      window.setTimeout(function(){
+        graph.redraw()
+      }, 500);
+    }
+  }
 
   $(document).ready(function(){
     var $reportableQuestion = $(".reportable_question");
     if ($reportableQuestion.length > 0){
       $reportableQuestion.each(function( index ){
-        var $pieChart = $(this).siblings(".pie-chart");
+        var $element = $(this).closest('.question').find('.chart');
         $.ajax({
           url: "/surveyable/questions/"+$(this).val()+"/reports",
           dataType: "json",
           success: function(results){
-            draw($pieChart, results);
+            drawGraph(results.type, results.questions, $element);
           }
         });
       });
     }
   });
-
 })(window);
