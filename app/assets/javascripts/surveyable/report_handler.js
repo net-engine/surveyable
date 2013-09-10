70c128e98fa85be3d2774f9675ee688cadf8a5c7
Brad Parker <hi@bradparker.com>
2013-09-10 10:41:20 +1000

    Adds ability to override graph options after Surveyable has been initiated

--- app/assets/javascripts/surveyable/report_handler.js
+++ app/assets/javascripts/surveyable/report_handler.js
@@ -1,63 +1,81 @@
-(function(w){
+var Surveyable = function(){
 
-  var drawGraph = function(type, data, $element, options) {
+  var self = this, drawGraph, type, options, data;
+
+  var barLineAndAreaRequired = {
+    xkey: 'answer_text',
+    ykeys: ['answer_occurrence'],
+    labels: ['Answer Occurrence']
+  }
+
+  var drawGraph = function(type, data, $element, options){
     var defaults = {
       element: $element,
       data: data
     }
+    var finalOptions = $.extend(defaults, options);
+    var graph = new Morris[type](finalOptions);
 
-    var final_options = $.extend(defaults, options);
-
-    var graph = new Morris[type](final_options);
-
-    window.onresize = function() {
-      window.setTimeout(function(){
-        graph.redraw()
-      }, 500);
-    }
+    // $(window).resize(function() {
+    //   window.setTimeout(graph.redraw, 500);
+    // });
   }
 
-  $(document).ready(function(){
-    var $reportableQuestion = $(".reportable_question");
-    if ($reportableQuestion.length > 0){
-      $reportableQuestion.each(function( index ){
-        var $element = $(this).closest('.question').find('.graph');
-        
-        $.ajax({
-          url: "/surveyable/questions/"+$(this).val()+"/reports",
-          dataType: "json",
-          success: function(results){
-            var type, options, data;
+  this.checkBoxFieldGraph = function(data, $element){
+    type = "Bar";
+    var inOptions = self.checkBoxFieldGraphOptions;
+    var outOptions = $.extend(inOptions, barLineAndAreaRequired)
 
-            if (results.field_type == "check_box_field") {
-              data = results.answers;
-              type = "Bar";
-              options = {
-                xkey: 'answer_text',
-                ykeys: ['answer_occurrence'],
-                labels: ['Answer Occurrence']
-              }
+    drawGraph(type, data, $element, outOptions);
+  }
+  
+  this.rankFieldGraph = function(data, $element){
+    type = "Line";
+    var inOptions = self.rankFieldGraphOptions;
+    var outOptions = $.extend(inOptions, barLineAndAreaRequired)
 
-            } else if (results.field_type == "rank_field") {
-              data = results.answers;
-              type = "Line";
-              options = {
-                xkey: 'answer_text',
-                ykeys: ['Answer Occurrence'],
-                labels: ['Value']
-              }
-            
-            } else if (results.field_type == "radio_button_field") {
-              type = "Donut"
-              data = $.map(results.answers, function(answer, index){
-                return { value: answer.answer_occurrence, label: answer.answer_text };
-              });
-            }
+    drawGraph(type, data, $element, outOptions);
+  }
+  
+  this.radioButtonFieldGraph = function(data, $element){
+    type = "Donut";
+    var outOptions = self.radioButtonFieldGraphOptions;
+    var outData = $.map(data, function(answer, index){
+      return { value: answer.answer_occurrence, label: answer.answer_text };
+    });
+    
+    drawGraph(type, outData, $element, outOptions);
+  }
 
-            drawGraph(type, data, $element, options);
+  return self;
+}
+
+var Surveyable = Surveyable();
+
+$(document).ready(function(){
+  var $reportableQuestion = $(".reportable_question");
+  
+  if ($reportableQuestion.length > 0){
+    $reportableQuestion.each(function( index ){
+      var $element = $(this).closest('.question').find('.graph');
+      
+      $.ajax({
+        url: "/surveyable/questions/"+$(this).val()+"/reports",
+        dataType: "json",
+        success: function(results){
+          if (results.field_type == "check_box_field") {
+            Surveyable.checkBoxFieldGraph(results.answers, $element);
+
+          } else if (results.field_type == "rank_field") {
+            Surveyable.rankFieldGraph(results.answers, $element);
+          
+          } else if (results.field_type == "radio_button_field") {
+            Surveyable.radioButtonFieldGraph(results.answers, $element);
+          
           }
-        });
+        }
       });
-    }
-  });
-})(window);
+    });
+  }
+});
+
