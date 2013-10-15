f60bffda9c40d47f47e5ee86dd09c37d4eddb0d8
Olivier Pichon <olivier@netengine.com.au>
2013-10-15 11:36:21 +1000

    See Trigger #11401: dropping a two columns index does not seem to work properly. We therefore run raw SQL in the database.

--- db/migrate/20131015001900_add_report_indexes.rb
+++ db/migrate/20131015001900_add_report_indexes.rb
@@ -2,7 +2,7 @@ class AddReportIndexes < ActiveRecord::Migration
   def up
     # this index speeds up a lot the often used query : survey.responses.completed
     # the order matters here: survey_id is more selective than completed_at not null
-    add_index :responses, [:survey_id, :completed_at]
+    add_index :responses, [:survey_id, :completed_at], name: "completed_responses"
 
     # this index speeds up the query based on question type such as
     # Question.where( field_type: Surveyable::Question::REPORTABLE_TYPES )
@@ -10,7 +10,11 @@ class AddReportIndexes < ActiveRecord::Migration
   end
 
   def down
-    remove_index :reponses, [:survey_id, :completed_at]
+    # dropping a two columns index does not seem to work properly
+    # we therefore run raw SQL in the database
+    execute <<-SQL
+        DROP INDEX "completed_responses";
+    SQL
     remove_index :questions,:field_type
   end
 end
