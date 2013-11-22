ea9ae4b062362399bec92cadc6265984c4d2090c
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-11-22 11:54:57 +1000

    Don't show incomplete responses

--- app/serializers/surveyable/csv/survey_serializer.rb
+++ app/serializers/surveyable/csv/survey_serializer.rb
@@ -15,7 +15,7 @@ module Surveyable
 
       def to_csv
         String.new.tap do |csv|
-          Surveyable::Response.where(id: response_ids).all.each do |response|
+          Surveyable::Response.where('completed_at IS NOT NULL').where(id: response_ids).all.each do |response|
             response_serializer = ResponseSerializer.new(response: response,
                                                          question_ids: question_ids)
             csv << response_serializer.to_csv
